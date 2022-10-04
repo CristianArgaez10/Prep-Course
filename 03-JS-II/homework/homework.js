2c3f8fbd99f16161dc069ac2ca1f3a470f1222d9
CristianArgaez10 <argaez.cristian.s @gmail.com>
2022-10-03 23:16:25 -0500

    Homeworks

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,31 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+  	return x;
+
+
+  }
+    if (y > x) {
+  	return y;
+  }
+  if (y = x) {
+  	return y;
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+  	var mayoriaDeEdad = "Allowed"
+  	return mayoriaDeEdad;
+  }
+    if (edad < 18) {
+  	var mayoriaDeEdad = "Not allowed"
+  	return mayoriaDeEdad;
+  }
 }
   
 function conection(status) {
@@ -19,6 +38,20 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+  	var status = "Online";
+  	return status;
+
+  }
+  if (status === 2) {
+  	var status = "Away";
+  	return status;
+  }
+  else {
+  	var status = "Offline";
+  	return status;
+
+  }
 }
 
 function saludo(idioma) {
@@ -28,6 +61,22 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+  	var saludo = "Guten Tag!";
+  	return saludo;
+  }
+  if (idioma === "mandarin") {
+  	var saludo = "Ni Hao!";
+  	return saludo;
+  }
+  if (idioma == "ingles") {
+  	var saludo = "Hello!";
+  	return saludo;
+  }
+  else {
+  	var saludo = "Hola!";
+  	return saludo;
+  }
 }
 
 function colors(color) {
@@ -38,18 +87,36 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+  	case "blue":
+  	return "This is blue";
+  	break;
+  	case "red":
+  	return "This is red";
+  	break;
+  	case "green":
+  	return "This is green";
+  	break;
+  	case "orange":
+  	return "This is orange";
+  	break;
+  	default:
+  	return "Color not found";
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  	return numero === 10 || numero === 5;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero < 50 && numero > 20;
 }
 
 function esEntero(numero) {
@@ -60,22 +127,50 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return numero % 1 === 0;
+
 }
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
+  if (numero % 15 === 0) {
+  	return "fizzbuzz";
+  }
   // Si "numero" es divisible entre 5, devuelve "buzz"
+  if (numero % 3 === 0) {
+  	return "fizz";
+  }
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+    if (numero % 5 === 0) {
+  	return "buzz";
+  }
+  else {
+  	return numero;
+  }
   // De lo contrario, devuelve el numero
 }
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
   //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+  	return "Error";
+  }
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+  	return "Hay negativos";
+  }
+  if (num1 > num2 && num1 > num3 && num1 > 0) {
+  	return "Número 1 es mayor y positivo";
+  }
+  if (num3 > num1 && num3 > num2) {
+  	return num3+1;
+  }
+  else {
+  	return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -84,12 +179,31 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2) {
+  	return false;
+  }
+  if (numero === 2) {
+  	return true;
+  }
+  for (var i = 2; i < numero; i++) {
+  	if (numero % i === 0) {
+  		return false;
+  	}
+  	}
+  	return true;
+
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === true) {
+  	return "Soy verdadero";
+  }
+  if (valor === false) {
+  	return "Soy falso";
+  }
 
 }
 
@@ -97,12 +211,22 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let arrayTablaDelSeis = []
+  for (let i = 0; i <=10; i++) {
+  	arrayTablaDelSeis.push(6*i)
+  }
+  return arrayTablaDelSeis;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if (numero > 99 && numero < 1000) {
+  	return true;
+  }
+  else {
+  	return false;
+  }
   
 }
 
@@ -110,6 +234,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var numero;
+  let i = 0;
+  do {
+  	i = i + 1;
+  	numero = numero + 5;
+  }
+  while (i < 8);
+  return numero;
 }
 
 
